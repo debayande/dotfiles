be7e1dc560f53aa3999d5ead3b5004d0ce397043
Debayan De <debayande@users.noreply.github.com>
2019-06-02 01:29:45 +0100

    Adds .sourcethis.zsh.

---
+++ .sourcethis.zsh
@@ -0,0 +1,11 @@
+#
+# .sourcethis.zsh
+#
+
+export EDITOR='subl -nw'
+export IGNOREEOF=2
+
+alias cask='brew cask'
+alias cb='cd $OLDPWD'
+alias nukehist='rm $HOME/.zsh_history; history -c'
+alias watch='watch '
