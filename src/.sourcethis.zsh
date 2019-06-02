36411a47385c10df2b063f5f51bb3c3e975740b5
Debayan De <debayande@users.noreply.github.com>
2019-06-02 01:29:45 +0100

    Adds src/.sourcethis.zsh.

---
+++ src/.sourcethis.zsh
@@ -0,0 +1,16 @@
+#!/opt/homebrew/bin/zsh
+
+# ; ${HOME}/.sourcethis.zsh for user `debayan` on `*.compute.feof.ovh`
+# ; Debayan De ‹debayan@feof.ovh›
+
+export PATH=/opt/homebrew/bin:${PATH}
+
+export AWS_SDK_LOAD_CONFIG="True"
+export CLOUDFLARE_API_KEY="‹POPULATE ME›"
+export CLOUDFLARE_EMAIL="‹POPULATE ME›"
+export EDITOR="subl -nw"
+export GPG_TTY=$(tty)
+export HISTFILE=${HOME}/.zsh_history
+export HISTSIZE=1000000
+export PIPENV_IGNORE_VIRTUALENVS=1
+export SAVEHIST=1000000
